16ad4925a7ad697262116ea277ae021468f4b8a4
king-open <kingwang.0624@gmail.com>
2024-12-21 03:57:31 +0800

    init

---
+++ SkyFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkyFinder
+//
+//  Created by 不二 Jack on 2024/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkyFinder/SkyFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyFinderApp.swift
+//  SkyFinder
+//
+//  Created by 不二 Jack on 2024/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SkyFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
